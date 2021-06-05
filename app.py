cad9c4f19d0734e2bd04610953f0ec9eef249306
azfar-bv-ai <azfarlari1999@gmail.com>
2021-06-05 16:17:13 +0530

    updated requirements

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ CORS(app)
 currentDirectory = os.getcwd() 
 databasePath = os.path.join(currentDirectory , "database.db")
 
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://vxdchuphjsryxr:8980947f072b81ab8109c1947e4f2fafda83d275dc02ca5ecb0b2e23c13740a5@ec2-184-73-198-174.compute-1.amazonaws.com:5432/d650arrdb8bgtn"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://vxdchuphjsryxr:8980947f072b81ab8109c1947e4f2fafda83d275dc02ca5ecb0b2e23c13740a5@ec2-184-73-198-174.compute-1.amazonaws.com:5432/d650arrdb8bgtn"
 db = SQLAlchemy(app) 
 
 import routes , models 
